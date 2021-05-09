81e1576918309ba464cfa74d9a043fabe2c66ed6
Estera <esterkastel@gmail.com>
2021-05-09 19:13:06 +0200

    Adding signalR

--- Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace Grapevine.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 1 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 2 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 3 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 4 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 5 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 6 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 7 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 8 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 9 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Grapevine.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\pvanderleur\source\repos\Grapevine\Client\_Imports.razor"
+#line 10 "C:\Users\eStelmach\privateProjects\Grapevine\Client\_Imports.razor"
 using Grapevine.Client.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Grapevine.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\pvanderleur\source\repos\Grapevine\Client\Shared\NavMenu.razor"
+#line 28 "C:\Users\eStelmach\privateProjects\Grapevine\Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
